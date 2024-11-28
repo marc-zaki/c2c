5607c68ff0c428de11bdc4ae11ac5ad4957e0ed8
Marc Zaki <immarkk7755@gmail.com>
2024-11-28 07:57:27 +0200

    Add files via upload
    updated javascript, and added pictures to the homescreen, and added a background image to the login page.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.querySelector('input[type="number"]').addEventListener('input', function (e) {
-    if (this.value.length > 14) {
-      this.value = this.value.slice(0, 14);
-      alert("Please input numbers")
-    }
-  });
+jQuery(document).ready(function($){
+$('.slider-image').on('click', function(){
+$('.slider-image').removeClass('active');
+$(this).addClass('active');
+})
+});
