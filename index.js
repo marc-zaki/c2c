f18ae543cbcbe7ea572787ad408a6e58a0f3b7f0
Marc <immarkk7755@gmail.com>
2024-11-28 23:24:23 +0200

    fixed some bugs with javascript; login page, and signup page

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", () =>{
+  const questions = document.querySelectorAll(".faq-question");
+  
+  questions.forEach(question => {
+    question.addEventListener("click", ()=> {
+      question.classList.toggle("active");
+      const answer = question.nextElementSibling;
+      const icon = question.querySelector(".icon");
+      
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+        icon.textContent = "+";
+      } else {
+        answer.style.display = "block";
+        icon.textContent ="-";
+      }
+    });
+  });
+  
+});
+
 $(document).ready(function(){
     $('.slider-image').on('click', function(){
         $('.slider-image').removeClass('active');
@@ -6,47 +27,37 @@ $(document).ready(function(){
 });
 
 document.getElementById('nationalID').addEventListener('input', function() {
-    var nationalID = this.value;
-    if (nationalID.length > 14) {
-        this.value = nationalID.slice(0, 14);
-    }
+  var nationalID = this.value;
+  if (nationalID.length > 14) {
+      this.value = nationalID.slice(0, 14);
+  }
 });
 
-function validateForm() {
-    var nationalID = document.getElementById('nationalID').value;
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirm-password').value;
+function signupValidate() {
+  var nationalID = document.getElementById('nationalID').value;
+  var password = document.getElementById('password').value;
+  var confirmPassword = document.getElementById('confirm-password').value;
 
-    if (nationalID.length !== 14) {
-        alert('National ID must be exactly 14 characters long.');
-        return false;
-    }
-
-    if (password !== confirmPassword) {
+  if (nationalID.length !== 14) {
+      alert('National ID must be exactly 14 characters long.');
+      return false;
+}
+  if (password !== confirmPassword) {
         alert('Passwords do not match.');
         return false;
-    }
-
+  }
     return true;
 }
-document.addEventListener("DOMContentLoaded", () =>{
-    const questions = document.querySelectorAll(".faq-question");
-    
-    questions.forEach(question => {
-      question.addEventListener("click", ()=> {
-        question.classList.toggle("active");
-        const answer = question.nextElementSibling;
-        const icon = question.querySelector(".icon");
-        
-        if (answer.style.display === "block") {
-          answer.style.display = "none";
-          icon.textContent = "+";
-        } else {
-          answer.style.display = "block";
-          icon.textContent ="-";
-        }
-      });
-    });
-    
-    
-  });
+
+function loginValidate() {
+  var nationalID = document.getElementById('nationalID').value;
+
+  if (nationalID.length !== 14) {
+      alert('National ID must be exactly 14 characters long.');
+      return false;
+  }
+
+  return true;
+}
+
+
