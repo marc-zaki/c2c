e9e852895a2b4c33839f0b4650413609428b65dc
Omar El banna <bannxa2005@gmail.com>
2024-11-28 22:20:02 +0200

    added faq page

--- index.js
+++ index.js
@@ -29,3 +29,24 @@ function validateForm() {
 
     return true;
 }
+document.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".faq-question");
+    
+    questions.forEach(question => {
+      question.addEventListener("click", ()=> {
+        question.classList.toggle("active");
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector(".icon");
+        
+        if (answer.style.display === "block") {
+          answer.style.display = "none";
+          icon.textContent = "+";
+        } else {
+          answer.style.display = "block";
+          icon.textContent ="-";
+        }
+      });
+    });
+    
+    
+  });
