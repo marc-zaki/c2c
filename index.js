7444d83fca120ea329564bdacef4485ce86c7e80
Marc <immarkk7755@gmail.com>
2024-11-28 18:36:58 +0200

    Added 2 scripts to the JS file, to make sure the ID is 14 characters long and the password and repeat passowrd match

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-jQuery(document).ready(function($){
-$('.slider-image').on('click', function(){
-$('.slider-image').removeClass('active');
-$(this).addClass('active');
-})
+$(document).ready(function(){
+    $('.slider-image').on('click', function(){
+        $('.slider-image').removeClass('active');
+        $(this).addClass('active');
+    });
 });
+
+document.getElementById('nationalID').addEventListener('input', function() {
+    var nationalID = this.value;
+    if (nationalID.length > 14) {
+        this.value = nationalID.slice(0, 14);
+    }
+});
+
+function validateForm() {
+    var nationalID = document.getElementById('nationalID').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirm-password').value;
+
+    if (nationalID.length !== 14) {
+        alert('National ID must be exactly 14 characters long.');
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match.');
+        return false;
+    }
+
+    return true;
+}
